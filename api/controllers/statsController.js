c4b01eec971614b078b4cc09b244ad223a554fba
Aaron Anderson <aaronlynanderson@gmail.com>
2017-09-10 19:32:08 -0400

    update_an_activity works

--- api/controllers/statsController.js
+++ api/controllers/statsController.js
@@ -24,7 +24,6 @@ exports.create_an_activity = function(req, res){
   });
 };
 
-// BUG: read_an_activity does not limit to a single response
 exports.read_an_activity = function(req, res){
   console.log("read_an_activity");
   Activities.find({activity:req.params.activity}, function(err, activity) {
@@ -34,9 +33,18 @@ exports.read_an_activity = function(req, res){
   });
 };
 
+exports.read_an_activity_by_id = function(req, res){
+  console.log("read_an_activity_by_id");
+  Activities.find({_id:req.params.id}, function(err, activity) {
+    if (err)
+      res.send(err);
+    res.json(activity);
+  });
+};
+
 exports.update_an_activity = function(req, res){
   console.log("update_an_activity");
-  Activities.findOneAndUpdate({_id: req.params.activityId}, req.body, {new: true}, function(err, activity) {
+  Activities.findOneAndUpdate({_id: req.params.id}, req.body, {new: true}, function(err, activity) {
     if (err)
       res.send(err);
     res.json(activity);
--- api/routes/statsRoutes.js
+++ api/routes/statsRoutes.js
@@ -3,15 +3,18 @@ module.exports = function(app) {
   const statsRoutes = require('../controllers/statsController');
 
   app.route('/activities')
-    .get(statsRoutes.list_all_activities)
-    .post(statsRoutes.create_an_activity);
+    .get(statsRoutes.list_all_activities) //works
+    .post(statsRoutes.create_an_activity); //works
 
-  app.route('/activities/:activity')
-    .get(statsRoutes.read_an_activity)
-    .put(statsRoutes.update_an_activity)
+  app.route('/activities/activity/:activity')
+    .get(statsRoutes.read_an_activity); //works
+
+  app.route('/activities/id/:id')
+    .get(statsRoutes.read_an_activity_by_id) //works
+    .put(statsRoutes.update_an_activity) //works
     .delete(statsRoutes.delete_an_activity);
 
-  app.route('/activities/{_id}/stats')
+  app.route('/activities/id/:id/stats')
     .post(statsRoutes.create_a_stat);
 
   app.route('/stats/:id')
