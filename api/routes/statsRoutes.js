b2244871275e88c82792cbfac383f2e7c2faf5bb
Aaron Anderson <aaronlynanderson@gmail.com>
2017-09-10 15:34:29 -0400

    Build out folder structure

---
+++ api/routes/statsRoutes.js
@@ -0,0 +1,27 @@
+app.get('/activities', function (req,res){
+
+});
+
+app.post('/activities', function (req,res){
+
+});
+
+app.get('/activities/:id', function (req,res){
+
+});
+
+app.put('/activites/:id', function (req,res){
+
+});
+
+app.delet('/activites/:id', function (req,res){
+
+});
+
+app.post('/activites/:id/stats', function (req,res){
+
+});
+
+app.delete('/stats/:id', function (req,res){
+
+});
--- server.js
+++ server.js
@@ -9,33 +9,6 @@ const app = express();
 mongoose.connect(mongoURL);
 const chalk = require('chalk');
 
-app.get('/activities', function (req,res){
-
-});
-
-app.post('/activities', function (req,res){
-
-});
-
-app.get('/activities/:id', function (req,res){
-
-});
-
-app.put('/activites/:id', function (req,res){
-
-});
-
-app.delet('/activites/:id', function (req,res){
-
-});
-
-app.post('/activites/:id/stats', function (req,res){
-
-});
-
-app.delete('/stats/:id', function (req,res){
-
-});
 
 app.listen(3000, function() {
    console.log(chalk.yellow.bgBlue.bold('Listening on port 3000'))
